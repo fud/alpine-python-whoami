f6d776376978e3ac66d62415e42cea4b5ac46127
Brenton Bills <Brenton.Bills@gmail.com>
2016-09-01 16:52:08 +1000

    Whoami docker container

---
+++ whoami.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+PORT = 8080
+
+class HelloWorldRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        message = socket.gethostname()
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+
+def run():
+
+    server_address = ("127.0.0.1", PORT)
+    httpd = HTTPServer(server_address, HelloWorldRequestHandler)
+
+    print("✓ Server running on port {}...".format(PORT))
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
